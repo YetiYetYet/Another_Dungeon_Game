ae87f8b9fec5ec8de7b5e269496dda17f145dab3
YetiYetYet <alexisbehier22@gmail.com>
2020-11-22 01:55:15 +0100

    Add Sword and AssaultRifles Item

---
+++ Assets/Scripts/OnPerspectiveSwitch.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using Opsive.UltimateCharacterController.Camera;
+using Opsive.UltimateCharacterController.Events;
+using UnityEngine;
+using Utils;
+
+public class OnPerspectiveSwitch : MonoBehaviour
+{
+    public CameraController cameraController;
+    // public Material invisibleMaterial;
+    public List<SkinnedMeshRenderer> invisibleWhenFPS;
+    public GameObject itemsParentRight;
+    public GameObject itemsParentLeft;
+
+    private void Start()
+    {
+        cameraController.OnChangePerspectivesEvent.AddListener(DisableMesh);
+    }
+
+    void DisableMesh(bool perspective)
+    {
+        MeshRenderer mesh;
+        foreach (var meshRenderer in invisibleWhenFPS)
+        {
+            meshRenderer.enabled = !perspective;
+        }
+
+        foreach (var child in Tools.GetChildRecursive(itemsParentRight.transform))
+        {
+            mesh = child.gameObject.GetComponent<MeshRenderer>();
+            if(mesh != null)
+                mesh.enabled = !perspective;
+            
+        }
+        
+        foreach (var child in Tools.GetChildRecursive(itemsParentLeft.transform))
+        {
+            mesh = child.gameObject.GetComponent<MeshRenderer>();
+            if(mesh != null)
+                mesh.enabled = !perspective;
+        }
+    }
+}
---
+++ Assets/Scripts/Utils/Tools.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Utils
+{
+    public static class Tools
+    {
+        public static List<Transform> GetChildRecursive(Transform root)
+        {
+            List<Transform> childs = new List<Transform>();
+            ChildBuildList(root, childs);
+            return childs;
+        }
+
+        private static void ChildBuildList(Transform root, List<Transform> list)
+        {
+            if (root.childCount > 0)
+            {
+                foreach (var child in root.GetComponentsInChildren<Transform>())
+                {
+                    list.Add(child);
+                    ChildBuildList(child, list);
+                }
+            }
+        }
+    }
+}
